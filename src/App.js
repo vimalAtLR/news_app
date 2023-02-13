4a81ce9f662445afe91da481818a8f64fb8e1b18
vimalAtLR <vimal@logicrays.com>
2023-02-13 16:10:43 +0530

    Implemented react-router-dom

--- src/App.js
+++ src/App.js
@@ -3,14 +3,24 @@ import './App.css';
 import React, { Component } from 'react';
 import NavBar from './components/NavBar';
 import News from './components/News';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+
 
 class App extends Component {
-  a = "Greetings"
   render() {
     return (
       <>
-        <NavBar />
-        <News pageSize={6}/>
+        <BrowserRouter>
+          <NavBar/>
+          <Routes>
+              <Route exact path="/" element={<News pageSize={6} country='in' category="general" key="general"/>} />
+              <Route exact path="/sport" element={<News pageSize={6} country='in' category="sport" key="sport"/>} />
+          </Routes>
+        </BrowserRouter>
       </>
     );
   }
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -5,7 +5,7 @@ export default class NewsItem extends Component {
         let { title, description, imgUrl, newsUrl } = this.props;
         return (
             <div className='my-3'>
-                <div className="card" style={{ width: "18rem"}} >
+                <div className="card" >
                     <img src={imgUrl?imgUrl:"https://www.yankodesign.com/images/design_news/2023/02/terms-like-military-grade-and-aircraft-grade-are-not-indicators-of-good-quality-heres-why/military_grade_aircragt_grade_yd_editorial_1.jpg"} className="card-img-top" alt="..." />
                     <div className="card-body">
                         <h5 className="card-title">{title}</h5>
